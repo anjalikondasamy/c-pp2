38eee2a4e222bfcf6a7823dd6405c8782faf9e23
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-23 16:43:19 +0530

    multiple13.c
    good

---
+++ multiple13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    clrscr();
+    
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+    printf("yes");
+    }
+    else
+    {
+    printf("no");
+    }
+    return 0;
+}
